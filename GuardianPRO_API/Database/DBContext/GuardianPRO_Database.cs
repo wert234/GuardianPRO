1f7b4bb8a979d65d57abd6daf175bea0cc34d6fc
Vladlen Damilin <danilinwladyslaw@gmail.com>
2023-03-07 23:30:18 +0400

    Добавьте файлы проекта.

---
+++ GuardianPRO_API/Database/DBContext/GuardianPRO_Database.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuardianPRO_API.Database.DBContext
+{
+    public class GuardianPRO_Database : DbContext
+    {
+        public GuardianPRO_Database(DbContextOptions options)
+            :base(options)
+        {
+        }
+    }
+}
